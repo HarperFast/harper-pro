88b4a8d4e71aa867f54ac3fa2a4e96757d0a8369
DavidCockerill <david@harperdb.io>
2026-05-07 09:47:26 -0400

    add cone node test fixtures

--- integrationTests/cloneNode/cloneNode.test.mjs
+++ integrationTests/cloneNode/cloneNode.test.mjs
@@ -1,10 +1,14 @@
 import { suite, test, before, after } from 'node:test';
 import { equal, ok } from 'node:assert';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { startHarper, teardownHarper } from '../../core/integrationTests/utils/harperLifecycle.ts';
 =======
 import { startHarper, teardownHarper, getNextAvailableLoopbackAddress, targz } from '@harperfast/integration-testing';
 >>>>>>> 1e6d174 (expand clone node to clone filesystem-only applications)
+=======
+import { startHarper, teardownHarper, getNextAvailableLoopbackAddress } from '@harperfast/integration-testing';
+>>>>>>> 6123298 (add cone node test fixtures)
 import { join } from 'node:path';
 import { setTimeout as sleep } from 'node:timers/promises';
 import { getNextAvailableLoopbackAddress } from '../../core/integrationTests/utils/loopbackAddressPool.ts';
