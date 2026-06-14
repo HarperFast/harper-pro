13c4102a5ffa2a7adbf0212b87f5ca06074780e6
github-action-benchmark <github@users.noreply.github.com>
2026-06-14 11:13:44 +0000

    add YCSB Cluster Latency p99 (customSmallerIsBetter) benchmark result for 9afa7ea758d05eb7b067e91af5b19f20bb40c3ab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781435622067,
+  "lastUpdate": 1781435624080,
   "repoUrl": "https://github.com/HarperFast/harper-pro",
   "entries": {
     "YCSB Cluster Throughput": [
@@ -52,6 +52,75 @@ window.BENCHMARK_DATA = {
             "name": "workload E",
             "value": 2034.5,
             "unit": "ops/sec"
+          }
+        ]
+      }
+    ],
+    "YCSB Cluster Latency p99": [
+      {
+        "commit": {
+          "author": {
+            "name": "harperfastaibot[bot]",
+            "username": "harperfastaibot[bot]",
+            "email": "280766738+harperfastaibot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "9afa7ea758d05eb7b067e91af5b19f20bb40c3ab",
+          "message": "feat: Sync Core (#365)\n\nCo-authored-by: kriszyp <34054+kriszyp@users.noreply.github.com>",
+          "timestamp": "2026-06-13T11:50:55Z",
+          "url": "https://github.com/HarperFast/harper-pro/commit/9afa7ea758d05eb7b067e91af5b19f20bb40c3ab"
+        },
+        "date": 1781435624059,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "C read p99",
+            "value": 7.17,
+            "unit": "ms"
+          },
+          {
+            "name": "B read p99",
+            "value": 62.19,
+            "unit": "ms"
+          },
+          {
+            "name": "B update p99",
+            "value": 70.41,
+            "unit": "ms"
+          },
+          {
+            "name": "A read p99",
+            "value": 19.9,
+            "unit": "ms"
+          },
+          {
+            "name": "A update p99",
+            "value": 21.35,
+            "unit": "ms"
+          },
+          {
+            "name": "F read p99",
+            "value": 71.51,
+            "unit": "ms"
+          },
+          {
+            "name": "F rmw p99",
+            "value": 147.26,
+            "unit": "ms"
+          },
+          {
+            "name": "E scan p99",
+            "value": 247.39,
+            "unit": "ms"
+          },
+          {
+            "name": "E insert p99",
+            "value": 145.48,
+            "unit": "ms"
           }
         ]
       }
