791a7a2ca73d53118a07ba2b39728e23e8bb6d60
github-action-benchmark <github@users.noreply.github.com>
2026-06-14 11:13:42 +0000

    add YCSB Cluster Throughput (customBiggerIsBetter) benchmark result for 9afa7ea758d05eb7b067e91af5b19f20bb40c3ab

---
+++ dev/bench/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1781435622067,
+  "repoUrl": "https://github.com/HarperFast/harper-pro",
+  "entries": {
+    "YCSB Cluster Throughput": [
+      {
+        "commit": {
+          "author": {
+            "name": "harperfastaibot[bot]",
+            "username": "harperfastaibot[bot]",
+            "email": "280766738+harperfastaibot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "9afa7ea758d05eb7b067e91af5b19f20bb40c3ab",
+          "message": "feat: Sync Core (#365)\n\nCo-authored-by: kriszyp <34054+kriszyp@users.noreply.github.com>",
+          "timestamp": "2026-06-13T11:50:55Z",
+          "url": "https://github.com/HarperFast/harper-pro/commit/9afa7ea758d05eb7b067e91af5b19f20bb40c3ab"
+        },
+        "date": 1781435621061,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "load",
+            "value": 10001.61,
+            "unit": "records/sec"
+          },
+          {
+            "name": "workload C",
+            "value": 22506.98,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "workload B",
+            "value": 8384.77,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "workload A",
+            "value": 9546.23,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "workload F",
+            "value": 4584.27,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "workload E",
+            "value": 2034.5,
+            "unit": "ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
